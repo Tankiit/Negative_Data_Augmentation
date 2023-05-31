e9b67a6ab53e61bd787c1b6dd3f8d3425cb9361a
Tanoy <mukherjee.tanmoy@gmail.com>
2023-05-31 15:18:12 +0200

    Add files via upload

---
+++ nda.py
@@ -0,0 +1,12 @@
+import torch
+from torchvision import datasets
+from torchvision.transforms import transforms
+
+
+
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+
+
+
+
